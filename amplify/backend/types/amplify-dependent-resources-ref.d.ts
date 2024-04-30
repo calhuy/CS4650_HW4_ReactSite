6fa8709ee9c83b6be3a5a805d4e9bbba4f486358
calhuy <calvinhooyen@gmail.com>
2024-04-30 11:57:18 -0700

    added authentication page (module 3)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cs4650hw4reactsitedfcfd454": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
